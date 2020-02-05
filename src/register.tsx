aaa459342a7dae84a23a04fb709aee0bb808cd96
Sergio M <smoreno@finametrix.com>
2020-02-05 17:22:35 +0100

    feat: now, stores selected theme on local-storage
    When reloading the page, it'll recover the previous theme you got selected

--- src/register.tsx
+++ src/register.tsx
@@ -1,10 +1,12 @@
-import addons from "@storybook/addons";
 import * as React from "react";
 import { Themes } from "./Themes";
+import { addons, types } from '@storybook/addons';
 
 addons.register("storybook/themes", (api) => {
-  addons.addPanel("storybook/themes/panel", {
-    render: ({ active }) => <Themes channel={addons.getChannel()} api={api} active={active} />,
+  addons.add("storybook/themes/panel", {
+    type: types.PANEL,
+    match: ({ viewMode }) => viewMode === 'story',
+    render: ({ active, key }) => <Themes key={key} channel={addons.getChannel()} api={api} active={active} />,
     title: "Themes",
   });
 });
